c0356720555b74e78a4a3057a0f37e99137acc49
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-06-13 09:56:09 +0200

    Add dummy routes for getting/posting new shot, logging out

--- app.js
+++ app.js
@@ -7,6 +7,24 @@ app.get('/', (request, response) => {
     response.send('Welcome to Your Best Shots!')
 })
 
+app.get('/dashboard', (request, response) => {
+    const IP = request.ip
+    response.send(`You\'re reaching out to us from ${IP}`)
+})
+
+app.get('/newshot', (request, response) => {
+    const route = request.route
+    response.send(`Where you're going: ${route}`)
+})
+
+app.post('/newshot', (request, response) => {
+    response.send('Thanks for submitting your new shot! You\'ll see it in your dashboard later!')
+})
+
+app.all('/logout', (request, response) => {
+    response.send('You\'ve been logged out :(')
+})
+
 app.listen(PORT, () => {
     console.log(`👋 Started espresso server on port ${PORT}`)
 })
