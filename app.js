d0f7eb31dcbf56754f55b50bd9a9c93ee2f36447
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-06-13 09:27:48 +0200

    Initialize project

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.get('/', (request, response) => {
+    response.send('Welcome to Your Best Shots!')
+})
+
+app.listen(PORT, () => {
+    console.log(`👋 Started server on port ${PORT}`)
+})
