fe01b43091cff079aaa4662c12643d6e5c6478cb
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-06-16 11:37:44 +0200

    Update shot schema and model

--- models/shot.js
+++ models/shot.js
@@ -3,11 +3,15 @@ import mongoose from 'mongoose'
 const shotSchema = new mongoose.Schema({
     id: { type: Number, unique: true, required: true },
     date: { type: Date, default: Date.now(), required: true },
-    beans: { type: String, required: true },
-    machine: { type: String, required: true },
-    grindsWeight: { type: Number, required: true },
-    shotsWeight: { type: Number, required: true },
-    comments: { type: String }
+    grindsWeightGrams: { type: Number, required: true },
+    shotsWeightGrams: { type: Number, required: true },
+    comments: { type: String },
+    favorite: { type: Boolean },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    bean: { type: Schema.Types.ObjectId, ref: 'Bean', required: true },
+    machine: { type: Schema.Types.ObjectId, ref: 'Maschine', required: true },
+    grinder: { type: Schema.Types.ObjectId, ref: 'Grinder' },
+
 })
 
 export const Shot = mongoose.model('Shot', shotSchema)
