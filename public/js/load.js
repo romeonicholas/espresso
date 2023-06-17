30ef0bfb2a8bdb1dc9dddeeda68c60e7ed807021
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-06-17 12:15:55 +0200

    Add more dark mode classes and toggle on dark mode select

--- public/js/load.js
+++ public/js/load.js
@@ -2,5 +2,11 @@ console.log('loaded')
 
 const btn = document.querySelector('.btn-toggle');
 btn.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme');
+    document.querySelector('.header').classList.toggle('dark-theme')
+    document.querySelector('.footer').classList.toggle('dark-theme')
+    document.querySelector('.aside').classList.toggle('dark-theme')
+    document.querySelector('.menu').classList.toggle('dark-theme')
+    document.querySelector('.main').classList.toggle('dark-theme')
+
+    console.log('Updated color theme')
 })
