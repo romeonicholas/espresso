d094f495deb090ba148af016561dcd355543a238
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-06-17 18:37:59 +0200

    Add login route username and password checking

--- controllers/users.js
+++ controllers/users.js
@@ -33,4 +33,31 @@ router.post(
     }
 )
 
+router.get('/login', (request, response) => {
+    response.render('users/login')
+})
+
+router.post(
+    '/login', 
+    body('username').isString().isLength({ max: 36 }).trim().escape(),
+    body('password').isString().isLength({ max: 256 }).trim().escape(),
+    async (request, response) => {
+        try {
+            const user = await User.findOne( { username: request.body.username }).exec()
+            if (!user) throw new Error ('User not found.')
+
+            bcrypt.compare(request.body.password, user.hashedPassword, function(err, result) {
+                if (result === true) { 
+                    response.redirect('/') 
+                } else {
+                    response.redirect('/')
+                }
+            });
+        } catch(error) {
+            console.error(error)
+            response.status(404).send('User could not be found')
+        }
+    }
+)
+
 export default router
