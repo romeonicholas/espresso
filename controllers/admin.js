57b1345975280bba9fea5c214b5972007494387e
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-06-22 13:41:17 +0200

    Pass unpublished objects to admin view

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,14 +1,26 @@
 import { Router } from 'express'
 import { User } from '../models/user.js'
+import { Bean } from '../models/bean.js'
+import { Machine } from '../models/machine.js'
+import { Grinder } from '../models/grinder.js'
 import { authenticateToken } from '../middlewares/authenticateToken.js'
 
 const router = Router()
 
-router.get('/unpublished', authenticateToken, (request, response) => {
+router.get('/unpublished', authenticateToken, async (request, response) => {
     if (!response.locals.isAdmin) {
         response.redirect('/')
     }
-    response.render('admin/unpublished')
+    const unpublishedBeans = await Bean.find( { isPublished: false }).exec()
+    const unpublishedMachines = await Machine.find( { isPublished: false }).exec()
+    const unpublishedGrinders = await Grinder.find( { isPublished: false }).exec()
+
+    response.render('admin/unpublished', 
+    { 
+        unpublishedBeans: unpublishedBeans,
+        unpublishedMachines: unpublishedMachines,
+        unpublishedGrinders: unpublishedGrinders
+    })
 })
 
 router.post('/unpublished', 
