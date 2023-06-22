0a33ecbad464d88a74105f60deff9679571c735a
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-06-22 12:30:40 +0200

    Add collapse to nav sections, clean up load js

--- public/js/load.js
+++ public/js/load.js
@@ -1,17 +1,31 @@
-const btn = document.querySelector('.slider')
 
-window.onload=function() {
-    if(localStorage.prefersDarkMode=='true') {
-        document.querySelectorAll('.header, .footer, .aside, .menu, .main')
-            .forEach((element) => { element.classList.toggle('dark-theme') })
-        
-        document.querySelector('.check').checked=true
-    } 
-  };
-
-btn.addEventListener('click', function() {
+if (localStorage.prefersDarkMode == 'true') {
     document.querySelectorAll('.header, .footer, .aside, .menu, .main')
         .forEach((element) => { element.classList.toggle('dark-theme') })
-    
-    localStorage.prefersDarkMode = (localStorage.prefersDarkMode=='true')?'false':'true'
+
+    document.querySelector('.check').checked = true
+}
+
+const darkModeButton = document.querySelector('.slider')
+
+darkModeButton.addEventListener('click', () => {
+    document.querySelectorAll('.header, .footer, .aside, .menu, .main')
+        .forEach((element) => { element.classList.toggle('dark-theme') })
+
+    localStorage.prefersDarkMode = (localStorage.prefersDarkMode == 'true') ? 'false' : 'true'
+})
+
+const collapseButtons = document.querySelectorAll(".collapse-button");
+
+collapseButtons.forEach((collapseButton) => {
+    collapseButton.addEventListener('click', () => {
+        collapseButton.classList.toggle("active");
+
+        let collapsibleContent = collapseButton.nextElementSibling;
+        if (collapsibleContent.style.maxHeight) {
+            collapsibleContent.style.maxHeight = null;
+        } else {
+            collapsibleContent.style.maxHeight = `${collapsibleContent.scrollHeight}px`;
+        }
+    });
 })
