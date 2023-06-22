f2f3eb73ffa19e586054d46ab1270ffb89b69b43
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-06-22 19:43:53 +0200

    Shift bean roastdates to array

--- models/bean.js
+++ models/bean.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose'
 const beanSchema = new mongoose.Schema({
     brand: { type: String, required: true },
     variety: { type: String, required: true },
-    roastDate: { type: Date },
+    roastDate: [{ type: Date }],
     isPublished: { type: Boolean, default: false, required: true }
 })
 
-beanSchema.index({ brand: 1, variety: 1, roastDate: 1 }, { unique: true })
+beanSchema.index({ brand: 1, variety: 1 }, { unique: true })
 
 export const Bean = mongoose.model('Bean', beanSchema)
