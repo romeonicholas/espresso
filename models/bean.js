c90625b60751d3ea9eef0f4827d927bb0a17b3ea
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-07-14 21:41:44 +0200

    Update bean model to be in line with other resources

--- models/bean.js
+++ models/bean.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose'
 
 const beanSchema = new mongoose.Schema({
     brand: { type: String, required: true },
-    variety: { type: String, required: true },
+    name: { type: String, required: true },
     isPublished: { type: Boolean, default: false, required: true }
 })
 
-beanSchema.index({ brand: 1, variety: 1 }, { unique: true })
+beanSchema.index({ brand: 1, name:1 }, { })
 
 export const Bean = mongoose.model('Bean', beanSchema)
