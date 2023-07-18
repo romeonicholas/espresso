bc356acb2690c3adbaa0254b1fb8b8d42d38d538
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-07-18 20:27:13 +0200

    Remove front page photo aside

--- controllers/simple-pages.js
+++ controllers/simple-pages.js
@@ -1,21 +1,12 @@
 import { Router } from "express"
-import got from "got"
 import { checkLoginStatus } from "../middlewares/checkLoginStatus.js"
 
 const router = Router()
 
 router.get("/", checkLoginStatus, async (request, response) => {
   try {
-    const coffeePhotoJSON = await got
-      .get("https://coffee.alexflipnote.dev/random.json")
-      .json()
-    let coffeePhotoLink = coffeePhotoJSON.file
-    if (!coffeePhotoLink) {
-      coffeePhotoLink = "https://sc.mogicons.com/c/177.jpg"
-    }
     response.render("index", {
       pageTitle: "Home",
-      coffeePhotoLink: coffeePhotoLink,
     })
   } catch (error) {
     console.error(error)
