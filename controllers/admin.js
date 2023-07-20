afae6e380f0ce78993f22e8eb97b591db702c858
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-07-20 16:15:40 +0200

    Optimize admin database access routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,18 +11,18 @@ router.get("/unpublished", authenticateToken, async (request, response) => {
     response.redirect("/")
   }
   try {
-    const unpublishedBeans = await Bean.find({ isPublished: false }).exec()
-    const unpublishedMachines = await Machine.find({
-      isPublished: false,
-    }).exec()
-    const unpublishedGrinders = await Grinder.find({
-      isPublished: false,
-    }).exec()
+    const unpublishedResourceTypes = [Bean, Machine, Grinder]
+
+    const unpublishedResources = await Promise.all(
+      unpublishedResourceTypes.map((resourceType) => {
+        return resourceType.find({ isPublished: false }).lean().exec()
+      })
+    )
 
     response.render("admin/unpublished", {
-      unpublishedBeans: unpublishedBeans,
-      unpublishedMachines: unpublishedMachines,
-      unpublishedGrinders: unpublishedGrinders,
+      unpublishedBeans: unpublishedResources[0],
+      unpublishedMachines: unpublishedResources[1],
+      unpublishedGrinders: unpublishedResources[2],
     })
   } catch (error) {
     console.error(error)
@@ -39,17 +39,21 @@ router.post(
     }
 
     try {
-      await Bean.updateMany(
-        { _id: { $in: request.body.beans } },
-        { isPublished: true }
-      )
-      await Machine.updateMany(
-        { _id: { $in: request.body.machines } },
-        { isPublished: true }
-      )
-      await Grinder.updateMany(
-        { _id: { $in: request.body.grinders } },
-        { isPublished: true }
+      const resourcesTuples = [
+        [Bean, "beans"],
+        [Machine, "machines"],
+        [Grinder, "grinders"],
+      ]
+
+      await Promise.all(
+        resourcesTuples.map((resourceTuple) => {
+          return resourceTuple[0]
+            .updateMany(
+              { _id: { $in: request.body[resourceTuple[1]] } },
+              { isPublished: true }
+            )
+            .exec()
+        })
       )
 
       response.redirect("/admin/unpublished")
@@ -69,17 +73,18 @@ router.post(
     }
 
     try {
-      await Bean.deleteMany(
-        { _id: { $in: request.body.beans } },
-        { isPublished: true }
-      )
-      await Machine.deleteMany(
-        { _id: { $in: request.body.machines } },
-        { isPublished: true }
-      )
-      await Grinder.deleteMany(
-        { _id: { $in: request.body.grinders } },
-        { isPublished: true }
+      const resourcesTuples = [
+        [Bean, "beans"],
+        [Machine, "machines"],
+        [Grinder, "grinders"],
+      ]
+
+      await Promise.all(
+        resourcesTuples.map((resourceTuple) => {
+          return resourceTuple[0]
+            .deleteMany({ _id: { $in: request.body[resourceTuple[1]] } })
+            .exec()
+        })
       )
 
       response.redirect("/admin/unpublished")
