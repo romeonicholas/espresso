08bc418f090b57cbb19755ed1c3a9d7d083a974c
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-07-20 09:27:41 +0200

    Add search views and simple search controller

--- controllers/search.js
+++ controllers/search.js
@@ -1,14 +1,25 @@
 import { Router } from "express"
+import { Shot } from "../models/shot.js"
 import { authenticateToken } from "../middlewares/authenticateToken.js"
 
 const router = Router()
 
-router.get("/", authenticateToken, async (request, response) => {
-  response.send("here is search!")
+router.get("/new", authenticateToken, async (request, response) => {
+  response.render("search/new")
 })
 
-router.post("/:searchTerms", authenticateToken, async (request, response) => {
-  response.send("You searched for: ", request.params.searchTerms)
+router.get("/results", authenticateToken, async (request, response) => {
+  let results = await Shot.find({
+    $text: {
+      $search: request.query.terms,
+    },
+  })
+    .populate("machine")
+    .populate("bean")
+    .populate("grinder")
+    .populate("user")
+
+  response.render("search/results", { results: results })
 })
 
 export default router
