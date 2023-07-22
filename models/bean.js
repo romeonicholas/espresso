3272f64e5da35e2e8783480b3f86cb6b385ff56d
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-07-22 11:01:17 +0200

    Add additional validations to bean and grinder schema

--- models/bean.js
+++ models/bean.js
@@ -1,8 +1,30 @@
 import mongoose from "mongoose"
 
 const beanSchema = new mongoose.Schema({
-  brand: { type: String, required: true },
-  name: { type: String, required: true },
+  brand: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 30,
+    validate: {
+      validator: (v) => {
+        return !v.includes("$")
+      },
+      message: "Brand cannot contain dollar sign",
+    },
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 30,
+    validate: {
+      validator: (v) => {
+        return !v.includes("$")
+      },
+      message: "Name cannot contain dollar sign",
+    },
+  },
   isPublished: { type: Boolean, default: false, required: true },
 })
 
