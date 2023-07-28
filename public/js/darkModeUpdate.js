a1cdc2c76c49e16ad3942c1c4487fc200a960e6c
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-07-28 17:45:43 +0200

    Add logic to open and close nav menu at breakpoint

--- public/js/darkModeUpdate.js
+++ public/js/darkModeUpdate.js
@@ -61,3 +61,41 @@ collapseButtons.forEach((collapseButton) => {
     }
   })
 })
+
+if (window.matchMedia("(min-width:720px)").matches) {
+  collapseButtons.forEach((collapseButton) => {
+    collapseButton.classList.add("active")
+
+    let collapsibleContent = collapseButton.nextElementSibling
+    collapsibleContent.style.maxHeight = `${collapsibleContent.scrollHeight}px`
+
+    let arrow = collapseButton.childNodes[1]
+    arrow.innerText = `⇡`
+  })
+}
+
+window.addEventListener("resize", () => {
+  if (window.matchMedia("(min-width:720px)").matches) {
+    collapseButtons.forEach((collapseButton) => {
+      collapseButton.classList.add("active")
+
+      let collapsibleContent = collapseButton.nextElementSibling
+      collapsibleContent.style.maxHeight = `${collapsibleContent.scrollHeight}px`
+
+      let arrow = collapseButton.childNodes[1]
+      arrow.innerText = `⇡`
+    })
+  }
+
+  if (window.matchMedia("(max-width:719px)").matches) {
+    collapseButtons.forEach((collapseButton) => {
+      collapseButton.classList.remove("active")
+
+      let collapsibleContent = collapseButton.nextElementSibling
+      collapsibleContent.style.maxHeight = null
+
+      let arrow = collapseButton.childNodes[1]
+      arrow.innerText = `⇣`
+    })
+  }
+})
