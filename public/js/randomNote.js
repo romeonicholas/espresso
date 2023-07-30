f0ad60b9848e87734cc02cc1eee4635a90c56240
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-07-30 11:00:46 +0200

    Fix typo in note

--- public/js/randomNote.js
+++ public/js/randomNote.js
@@ -2,7 +2,7 @@ const notes = [
   "Use ~14-18 grams of coffee beans for a doble. Use ~7-9 grams for a single.",
   "Use good water! The best is spring water, but distilled water works too.",
   "Distribute your grounds evenly in the filter basket. Only lightly tap the portafilter.",
-  "Makes sure to lock the portafilter onto the brew head of your machine to create a tight seal.",
+  "Make sure to lock the portafilter onto the brew head of your machine to create a tight seal.",
   "The grind size for espresso is smaller than for drip coffee, closer to table salt in size.",
   "Aim for about 30 pounds of pressure when tamping yoru grinds.",
   "Aim for a 25 seconds extraction time to start, and lengthen or shorten from there.",
