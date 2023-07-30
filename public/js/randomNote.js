20c5eb8bfdf638c2d5766e15d2ccdc33369b422d
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2023-07-30 11:07:04 +0200

    Fix typos, add goat styling

--- public/js/randomNote.js
+++ public/js/randomNote.js
@@ -1,11 +1,11 @@
 const notes = [
-  "Use ~14-18 grams of coffee beans for a doble. Use ~7-9 grams for a single.",
-  "Use good water! The best is spring water, but distilled water works too.",
-  "Distribute your grounds evenly in the filter basket. Only lightly tap the portafilter.",
-  "Make sure to lock the portafilter onto the brew head of your machine to create a tight seal.",
-  "The grind size for espresso is smaller than for drip coffee, closer to table salt in size.",
-  "Aim for about 30 pounds of pressure when tamping yoru grinds.",
-  "Aim for a 25 seconds extraction time to start, and lengthen or shorten from there.",
+  "Use 14-18 grams of coffee beans for a doble, or 7-9 grams for a single.",
+  "Use good water! Spring water is best, but distilled water works too.",
+  "Distribute your grounds evenly in the filter basket, only lightly tap the portafilter.",
+  "Lock the portafilter onto the brew head of your machine to create a tight seal.",
+  "Espresso is ground smaller than for drip coffee, closer to table salt in size.",
+  "Aim for about 30 pounds of pressure when tamping your grinds.",
+  "Start with a 25 second extraction time, and lengthen or shorten from there.",
 ]
 
 const noteIndex = Math.floor(Math.random() * notes.length)
